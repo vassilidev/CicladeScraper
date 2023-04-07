bb0a689b13a75377de14af6910315b095714a9ca
vassilidev <vassilidevnet@gmail.com>
2023-04-07 14:20:26 +0200

    Last version of the scraper with logging and working output

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Ciclade Scraper
+
+
+Find lost bank account from csv.
+## Tech Stack
+
+**Node:** v16.14.2
+
+**NPM:** 9.6.3
+
+
+## Installation
+
+```bash
+  git clone git@github.com:vassilidev/CicladeScraper.git
+  cp .env.exemple .env
+  npm i
+  node index.js
+```
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`CICLADE_URL` (https://ciclade.caissedesdepots.fr/je-lance-ma-recherche)
+
+`INPUT_PATH` (input.csv)
+
+`OUTPUT_PATH` (output.json)
+
+`CAPTCHA_TOKEN` (https://2captcha.com/)
+
+
+## Authors
+
+- [@vassilidev](https://www.github.com/vassilidev)
+
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import fs from 'fs';
 import csvParser from "csv-parser";
 import RecaptchaPlugin from "puppeteer-extra-plugin-recaptcha";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import * as winston from 'winston';
 
 dotenv.config();
 
@@ -16,14 +17,40 @@ puppeteer.use(
     StealthPlugin(),
 );
 
+const logConfiguration = {
+    'transports': [
+        new winston.transports.File({
+            filename: 'logs/run-' + new Date().toJSON().replaceAll(':', '-') + '.log',
+        }),
+    ],
+};
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
+let logger = winston.createLogger(logConfiguration);
+
 (async () => {
+    logger.info('Start browser');
+
     const browser = await puppeteer.launch({headless: false, ignoreHTTPSErrors: true});
     const page = await browser.newPage();
+
+    await page.setDefaultNavigationTimeout(0);
+
+    await page.exposeFunction('logger', message => logger.info(message))
+
+    logger.info('Set User Agent');
 
     await page.setUserAgent(new UserAgent().toString());
 
     const rows = [];
     const results = [];
+
+    logger.info('Read input file');
 
     fs.createReadStream(process.env.INPUT_PATH)
         .pipe(csvParser())
@@ -34,15 +61,22 @@ puppeteer.use(
      * Read all the lines and loop though.
      */
     async function parseData() {
+        logger.info('Start parsing data');
+
         for (const row of rows) {
+            logger.info('Parse data for the row ' + row.ref_dossier);
+
             await getData(row);
         }
 
-        // Log into console the result.
-        console.log(results);
+        logger.info('Closing the browser');
+
+        await browser.close();
+
+        logger.info('Write the output');
 
         await fs.writeFile(process.env.OUTPUT_PATH, JSON.stringify(results), err => {
-            console.log(err);
+            logger.error(err);
         });
 
         return results;
@@ -54,9 +88,13 @@ puppeteer.use(
      * @param row
      */
     async function getData(row) {
+        logger.info('Going to ' + process.env.CICLADE_URL);
+
         await page.goto(process.env.CICLADE_URL);
 
         await page.evaluate((row, process) => {
+            logger('Accept cookie');
+
             if (document.querySelector(process.env.ACCEPT_COOKIE_SELECTOR)) {
                 document.querySelector(process.env.ACCEPT_COOKIE_SELECTOR).click();
             }
@@ -81,7 +119,7 @@ puppeteer.use(
 
             // Gender
 
-            if (row.gender === 'mr') {
+            if (row.gender === 'H') {
                 document.querySelector(process.env.CIVILITY_MR_SELECTOR).checked = true;
                 document.querySelector(process.env.CIVILITY_MME_SELECTOR).checked = false;
             } else {
@@ -94,12 +132,10 @@ puppeteer.use(
             document.querySelector(process.env.BIRTH_SURNAME_SELECTOR).value = row.birth_surname;
             document.querySelector(process.env.USE_SURNAME).value = row.use_surname;
 
-            let names = row.name.split('|');
-
-            document.querySelector(process.env.NAME_SELECTOR).value = names[0];
-            document.querySelector(process.env.OTHER_NAME_1_SELECTOR).value = names[1] ?? row.other_name1 ?? '';
-            document.querySelector(process.env.OTHER_NAME_2_SELECTOR).value = names[2] ?? row.other_name2 ?? '';
-            document.querySelector(process.env.OTHER_NAME_3_SELECTOR).value = names[3] ?? row.other_name3 ?? '';
+            document.querySelector(process.env.NAME_SELECTOR).value = row.name ?? '';
+            document.querySelector(process.env.OTHER_NAME_1_SELECTOR).value = row.other_name1 ?? '';
+            document.querySelector(process.env.OTHER_NAME_2_SELECTOR).value = row.other_name2 ?? '';
+            document.querySelector(process.env.OTHER_NAME_3_SELECTOR).value = row.other_name3 ?? '';
 
             // Birth
 
@@ -118,8 +154,12 @@ puppeteer.use(
             document.querySelector('input[id=edit-numero-de-reference-0]').checked = true;
         }, row, process);
 
+        logger.info('Waiting for captcha');
+
         // Bypass the re-captcha
         await page.solveRecaptchas();
+
+        logger.info('Solved ! Submitting...');
 
         // Submit the form
         await page.click(process.env.FIRST_SUBMIT_SELECTOR);
@@ -127,18 +167,26 @@ puppeteer.use(
         // Wait the new page
         await page.waitForNavigation();
 
+        logger.info('Waiting result...');
+
         // Submit the last form
         await page.click(process.env.LAST_SUBMIT_SELECTOR);
 
         // Wait for the result
-        await page.waitForNavigation();
+        await delay(1500);
 
-        // Take a screenshot of the result
-        await page.screenshot({path: 'response-' + Date.now() + '.png', fullPage: true});
+        let isWorking = await page.evaluate(function (process) {
+            return !document.querySelector(process.env.OUTPUT_SELECTOR).textContent.includes(process.env.OUTPUT_DONT_WORK);
+        }, process);
 
-        results.push({
-            toto: 'titi',
-        });
+        if (isWorking) {
+            logger.info('GG ! ' + row.ref_dossier);
+
+            results.push(row);
+        } else {
+            logger.info(':( Nothing for ' + row.ref_dossier);
+        }
+
+        return results;
     }
-
 })();
